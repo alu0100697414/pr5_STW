441742f50bc01355a9b31f93822c0ec4b32704b2
alu0100697414 <alu0100697414@ull.edu.es>
2015-11-01 20:13:44 +0000

    Probando express

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 var express = require('express');
+var path = require('path');
+
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var expressLayouts = require('express-ejs-layouts');
+app.set('layout', 'layout');
+
+app.use(express.static('.'));
+app.use(expressLayouts);
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.set('port', (process.env.PORT || 8080));
+
+app.get('/', function(req, res){
+  res.render('index');
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+app.post('/res', function(req, res){
+  //res.send('Prueba: ' + req.body.ini_temp);
+  resultado.innerHTML = req.body.ini_temp;
+  res.render('index');
+});
 
-  console.log('Example app listening at http://localhost:%s', port);
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
---
+++ temp.js
@@ -0,0 +1,63 @@
+"use strict"; // Sirve para dar mensajes de error y asi encontrarlos antes.
+
+// Clase Medida
+function Medida() {
+  this.valor_;
+  this.tipo_;
+}
+
+// Clase Temperatura
+function Temperatura() {
+  Medida.call(this);
+}
+
+// Indicamos que Temperatura hereda de Medida.
+Temperatura.prototype = new Medida();
+
+
+// Inicializador
+Temperatura.prototype.inicializador = function(temp){
+  var exp_regular = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([fFcC])/;
+  var valor = temp.match(exp_regular);
+
+  this.set_valor(parseFloat(valor[1]));
+  this.set_tipo(valor[2]);
+}
+
+
+// Setters y Getters de la clase Medida
+Medida.prototype.get_valor = function(){
+  return this.valor_;
+}
+
+Medida.prototype.get_tipo = function(){
+  return this.tipo_;
+}
+
+Medida.prototype.set_valor = function(valor){
+  this.valor_ = valor;
+}
+
+Medida.prototype.set_tipo = function(tipo){
+  this.tipo_ = tipo;
+}
+
+Temperatura.prototype.conversor = function(){
+
+  if(this.get_tipo() === 'C' || this.get_tipo() ==='c'){
+    var res = (this.get_valor()*(9/5))+32;
+    return ("El resultado es: " + res + " " + "F");
+  }
+
+  else {
+    var res = (this.get_valor()-32)*(5/9);
+    return ("El resultado es: " + res + " " + "C");
+  }
+}
+
+
+function convertir(){
+  var temperatura = new Temperatura();
+  temperatura.inicializador(inicial.value);
+  resultado.innerHTML = temperatura.conversor();
+}
